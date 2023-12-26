1f0cc0c7861955c6265191629a51ad5ae0aa1ad5
Alejandro Front <desarrollo@qsocialnow.com>
2023-12-26 11:48:43 -0300

    arreglo login

--- src/contenedores/Dashboard/Filtros.jsx
+++ src/contenedores/Dashboard/Filtros.jsx
@@ -530,7 +530,7 @@ const disabledDate = current => {
   }
   return (
     <div>
-    <div className='nombreDashboard'>ARG - Movilizacion -MOV  {modeloSinEspacios ? `- ${modeloSinEspacios}` : null}</div>
+    <div className='nombreDashboard'>ARG- Movilizacion - Mov {modeloSinEspacios ? `- ${modeloSinEspacios}` : null}</div>
     <div className='contenedor-filtros'>
      <div className='boton-informe'>
      <Tooltip placement="top" title='Generar informe' >
